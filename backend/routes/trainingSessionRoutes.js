2cb6df98d7d2359ef01d3c1485de516aac52662e
YihReng <yihrengtan@gmail.com>
2024-07-30 22:18:06 +0800

    group session table by session id

--- backend/routes/trainingSessionRoutes.js
+++ backend/routes/trainingSessionRoutes.js
@@ -6,8 +6,41 @@ const router = express.Router();
 router.use(protect);    //add this
 
 router.get("/", async (req, res) => {
-    const trainingSessions = await getTrainingSessions();
-    return res.status(200).send(trainingSessions);
+    const all = await getTrainingSessions();
+    const trainingSessionsDict = {};
+    
+    all.forEach(session => {
+      // if trianingSessionsDict does not have the session id, create populate the trainingsessiondict with the details of the session
+      if (!trainingSessionsDict[session.session_id]) {
+        trainingSessionsDict[session.session_id] = {
+          session_id: session.session_id,
+          start_date: session.start_date,
+          end_date: session.end_date,
+          expiry_date: session.expiry_date,
+          training_title: session.training_title,
+          training_id: session.training_id,
+          employees: [
+            {
+              employee_id: session.employee_id,
+              employee_name: session.employee_name,
+              designation: session.designation,
+              status: session.status,
+            }
+          ]
+        };
+      }
+      // just add the employee details to the employees attribute of the session
+      else {
+        trainingSessionsDict[session.session_id].employees.push({
+          employee_id: session.employee_id,
+          employee_name: session.employee_name,
+          designation: session.designation,
+          status: session.status,
+        });
+      }
+    });
+
+    return res.status(200).send(trainingSessionsDict);
 })
 
 router.post("/", async(req, res) => {
