7b8b21fe0297ecad11b86e65dea34ed5aa3f1a32
Vin11704 <malvin.ken.s@gmail.com>
2024-08-07 18:06:34 +0800

    remove role data on tests

--- backend/test/loginDatabaseIntegration.test.js
+++ backend/test/loginDatabaseIntegration.test.js
@@ -13,7 +13,7 @@ describe('Integration test: Login functions', () => {
          console.log(testUsername, testPassword);
           const hashedPassword = await bcrypt.hash(testPassword, 10); //hash first before inserting database
           await pool.query(
-              'INSERT INTO user_credentials (username, password, role) VALUES (?, ?, ?)',
+              'INSERT INTO user_credentials (username, password) VALUES (?, ?)',
               [testUsername, hashedPassword, 'hr']
             );
           });
